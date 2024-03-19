b157a67d31836347d4c5f2f5835adec37ee8c6aa
gukazma <gukazma@proton.me>
2024-03-19 14:52:47 +0800

    ✨ feat: 输入输出的tool触发

--- Apps/DemoApp/MainWindow.cpp
+++ Apps/DemoApp/MainWindow.cpp
@@ -3,12 +3,27 @@
 #include <QDropEvent>
 #include <QMimeData>
 #include <iostream>
+#include <QFileDialog>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     setAcceptDrops(true);
+    connect(ui->toolButton_input, &QToolButton::clicked, [=]() {
+        auto inputFile = QFileDialog::getOpenFileName(
+            nullptr, QString::fromLocal8Bit("航点文件输入路径"), "", "(*.txt)");
+        if (!inputFile.isEmpty()) {
+            ui->lineEdit_input->setText(inputFile);
+        }
+        });
+    connect(ui->toolButton_output, &QToolButton::clicked, [=]() {
+        auto outputFile = QFileDialog::getSaveFileName(
+            nullptr, QString::fromLocal8Bit("航点输出文件路径"), "output.txt", "(*.txt)");
+        if (!outputFile.isEmpty()) {
+            ui->lineEdit_ouput->setText(outputFile);
+        }
+    });
 }
 
 MainWindow::~MainWindow()
@@ -20,12 +35,7 @@ void MainWindow::dropEvent(QDropEvent* event_) {
     const QUrl    url        = event_->mimeData()->urls().at(0);
     const QString inputFile = url.toLocalFile();
     std::cout << "filename : " << inputFile.toLocal8Bit().toStdString() << std::endl;
-    try {
-
-    }
-    catch (const std::exception& e) {
-
-    }
+    ui->lineEdit_input->setText(inputFile);
     event_->acceptProposedAction();
 }
 
